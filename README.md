769dd98d49c8cfafd63b13e8642e4a35efddcad0
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-01-30 13:31:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# javascript_cash-to-coins
---
+++ cashToCoins.js
@@ -0,0 +1,17 @@
+
+// Convert the dollar amount into the coins that make up that dollar amount. The final result is an object with the correct number of quarters, dimes, nickels, and pennies.
+const dollarAmount = 0.10
+const piggyBank = {}
+
+// Your magic code here
+
+console.log(piggyBank)
+
+// That should produce the following output.
+
+// {
+//   quarters: 0,
+//   dimes: 1,
+//   nickels: 0,
+//   pennies: 0
+// }
